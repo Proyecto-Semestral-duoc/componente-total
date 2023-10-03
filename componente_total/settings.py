9a019beac150f35af4afebfc3fee8a1611e6e0c2
Andronsio <102746620+Andronsio@users.noreply.github.com>
2023-10-02 22:10:47 -0300

    Revert "Merge branch 'main' of https://github.com/Proyecto-Semestral-duoc/componente-total"
    This reverts commit 51d96e798933244a9315e4646dac63e8d925e651, reversing
    changes made to 99523e5b01826acba4d4ea51a0c4918d92313763.

--- componente_total/settings.py
+++ componente_total/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8vk-w$q%+p6o83k55au)#^dbrgl-e5wgpgfe^b(kkpb3bd+dw*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
