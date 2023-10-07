6ae5494f24ebc98795b09dbccc247db5e3b33549
Dieguito <105881222+diegoext514@users.noreply.github.com>
2023-10-07 18:39:36 -0300

    update admin

--- core/admin.py
+++ core/admin.py
@@ -4,3 +4,6 @@ from .models import *
 # Register your models here.
 admin.site.register(Producto)
 admin.site.register(Factura)
+admin.site.register(OrdenCompra)
+admin.site.register(Region)
+admin.site.register(Comuna)
